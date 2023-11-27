fad36f6cc6e178d2b0489435833a650470d62d35
Vaidik Nimavat <vd386827@dal.ca>
2023-11-27 16:30:46 -0400

    punchout API chnages

--- server-app/src/api/controllers/timeSheet/punchOut.js
+++ server-app/src/api/controllers/timeSheet/punchOut.js
@@ -3,27 +3,41 @@ const response = require("../../../utils/response");
 
 async function punchOut(req, res) {
   try {
-    const { _id, endTime } = req.body;
+    const { _id, startTime, endTime } = req.body;
     const existingTimesheet = await Timesheet.findById(_id);
 
     if (!existingTimesheet) {
       return response(res, 404, false, { message: "Timesheet not found" });
     }
 
+    existingTimesheet.startTime = startTime;
     existingTimesheet.endTime = endTime;
     existingTimesheet.totalHours = calculateTotalHours(
       existingTimesheet.startTime,
-      endTime
+      existingTimesheet.endTime
     );
 
-    if (isApprovalNeeded(existingTimesheet.startTime, endTime)) {
-      existingTimesheet.approvalNeeded = true;
-    }
+    const approvalNeeded = isApprovalNeeded(existingTimesheet.startTime, existingTimesheet.endTime);
+    existingTimesheet.approvalNeeded = approvalNeeded;
+
+    if (existingTimesheet.totalHours > 50) {
+        existingTimesheet.isOverTime = true;
+        existingTimesheet.isApproved = false;
+      } else {
+        existingTimesheet.isOverTime = false;
+        existingTimesheet.isApproved = true;
+      }
 
     await existingTimesheet.save();
+
+    const responseMessage = approvalNeeded
+      ? "Punch out successful. Approval needed."
+      : "Punch out successful. Approval not needed.";
+
     return response(res, 200, true, {
-      message: "Punch out successful",
+      message: responseMessage,
       timesheetId: existingTimesheet._id,
+      approvalNeeded: approvalNeeded,
     });
   } catch (error) {
     console.error("Error punching out", error);
@@ -34,14 +48,22 @@ async function punchOut(req, res) {
 }
 
 function calculateTotalHours(startTime, endTime) {
-  const millisecondsInHour = 3600000;
-  const totalMilliseconds = new Date(endTime) - new Date(startTime);
+  const millisecondsInHour = 3600;
+  const startDateTime = new Date(startTime);
+  const endDateTime = new Date(endTime);
+
+  if (endDateTime < startDateTime) {
+    return 0;
+  }
+
+  const totalMilliseconds = endDateTime - startDateTime;
   return totalMilliseconds / millisecondsInHour;
 }
 
 function isApprovalNeeded(startTime, endTime) {
   const maxHoursWithoutApproval = 50;
   const totalHours = calculateTotalHours(startTime, endTime);
+  console.log("TOTAL HRS======>", totalHours);
   return totalHours > maxHoursWithoutApproval;
 }
 
