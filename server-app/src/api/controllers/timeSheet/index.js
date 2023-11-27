d4d2d325ca40ca38fafe34a8f19ea8c8f632589e
MANN MISTRY <mn732058@dal.ca>
2023-11-27 16:12:21 -0400

    Added: entry of details.js added to index.js

--- server-app/src/api/controllers/timeSheet/index.js
+++ server-app/src/api/controllers/timeSheet/index.js
@@ -1,7 +1,8 @@
 const addPunch = require("./addPunch");
 const updatePunch = require("./updatePunch");
-
+const details = require("./details");
 module.exports = {
   addPunch,
   updatePunch,
+  details,
 };
