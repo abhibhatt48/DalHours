b98ff40b8622d07992da0ab3ea7386a50f31c543
Vaidik Nimavat <vd386827@dal.ca>
2023-11-27 19:22:25 -0400

    Adding router file

--- server-app/src/api/routes/timeSheet/index.js
+++ server-app/src/api/routes/timeSheet/index.js
@@ -6,6 +6,6 @@ router.post("/add-punch", punchController.addPunch);
 router.patch("/update-punch", punchController.updatePunch);
 router.post("/punchout", punchController.punchOut);
 router.get("/details", punchController.details);
-
+router.post("/approve", punchController.approve);
 
 module.exports = router;
