71f49f2ee7554e8e2bc6ee02143f84f81400af55
Roshil Ka Patel <roshilkapatel2000@gmail.com>
2023-11-28 14:19:38 -0400

    Bug fix in course Details page

--- client-app/src/config/Axios.js
+++ client-app/src/config/Axios.js
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://10.0.2.2:4000/',
-  // baseURL: 'http://134.41.58.54:4000/',
+  // baseURL: 'http://10.0.2.2:4000/',
+  baseURL: 'http://134.190.131.33:4000/',
   // baseURL: 'http://127.0.0.1:4000/',
 });
 
