6dd51ed81cdc0c580287dd0d1312e23ab17020fc
Vaidik Nimavat <vd386827@dal.ca>
2023-12-01 16:42:55 -0400

    Add Test cases files

--- server-app/src/api/controllers/course/registerCourse.js
+++ server-app/src/api/controllers/course/registerCourse.js
@@ -11,13 +11,11 @@ const registerCourse = async (req, res) => {
       members,
     } = req.body;
 
-    // Check if the course with the given courseId already exists
     const existingCourse = await Course.findOne({ name, CRN });
     if (existingCourse) {
       return response(res, 409, false, { message: "Course already exists" });
     }
 
-    // Create a new course instance
     const newCourse = new Course({
       name,
       CRN,
@@ -26,10 +24,8 @@ const registerCourse = async (req, res) => {
       members,
     });
 
-    // Save the course to the database
+    
     await newCourse.save();
-
-    // Respond with a success message
     return response(res, 201, true, {
       message: "Course registered successfully",
       courseId: newCourse._id,
